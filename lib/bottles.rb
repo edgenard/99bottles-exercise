848a7c82e1d71bc7b4ca87a6fe568f67f88bcf07
egenard <egenard@2u.com>
2017-08-09 09:54:27 -0400

    Playing with Flog score, it went up with less code :shrugs:

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,29 +1,42 @@
 class Bottles
+  def song
+    verses(99, 0)
+  end
+
+  def verses(*numbers)
+    numbers.first.downto(numbers.last).to_a.map { |number| verse(number) }.join("\n")
+  end
+
   def verse(number)
+    case number
+      when 0
+       last_verse
+      else
+        regular_verse(number)
+    end
+  end
+
+
+  def last_verse
+    <<-Verse
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+Verse
+  end
+  
+  def regular_verse(number)
     <<-Verse
 #{bottle_phrase(number)} of beer on the wall, #{bottle_phrase(number)} of beer.
-#{take_phrase(number - 1)} and pass it around, #{bottle_phrase(number - 1)} of beer on the wall.
+#{take_down_phrase((number == 1))} and pass it around, #{bottle_phrase(number - 1)} of beer on the wall.
     Verse
   end
 
-
-  def first_line_of_verse
-
-  end
-
-  def second_line_of_verse
-
-  end
-
   def bottle_phrase(number)
-    return "#{number} bottles" if number > 1
-    return "no more bottles" if number < 1
-    "#{number} bottle"
+    return "#{number} bottle#{'s' if number > 1}" if number > 0
+     "no more bottles"
   end
 
-  def take_phrase(number)
-    return "Take one down" if number >= 0
-    return "Go to the store and buy some more, 99 bottles of beer on the wall." if number < 0
-    "Take it down"
+  def take_down_phrase(last_beer = true)
+    "Take #{last_beer ? 'it' : 'one'} down"
   end
 end
